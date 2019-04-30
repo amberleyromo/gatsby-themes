a3953327554629f03c13199558644871a285deef
Amberley Romo <amberleyjohanna@gmail.com>
2019-04-30 15:53:25 -0500

    gatsby-theme-darklight-blog: rm feed plugin

--- packages/gatsby-theme-darklight-blog/gatsby-config.js
+++ packages/gatsby-theme-darklight-blog/gatsby-config.js
@@ -106,7 +106,6 @@ module.exports = {
         // trackingId: `ADD YOUR TRACKING ID HERE`,
       }
     },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
